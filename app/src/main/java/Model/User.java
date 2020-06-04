8565c98ce13bb295ce2a8b850e8f4ff5daef3dcb
zhenglize <zhenglizeA@126.com>
2020-06-04 18:38:50 +0800

    实体类

---
+++ app/src/main/java/Model/User.java
@@ -0,0 +1,25 @@
+package Model;
+/**
+    @author zhenglize
+    实体类
+*/
+public class User {
+    String photo;
+    String description;
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
