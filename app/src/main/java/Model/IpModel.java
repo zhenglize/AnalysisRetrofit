6e49806edda48fc89f4ebe27187a3a2c5c6deb27
zhenglize <zhenglizeA@126.com>
2020-06-04 16:44:59 +0800

    实体类对象

---
+++ app/src/main/java/Model/IpModel.java
@@ -0,0 +1,25 @@
+package Model;
+/**
+    @author zhenglize
+   Retrofit请求返回的对象的实体类
+*/
+public class IpModel {
+    private int code;
+    private IpData data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public IpData getData() {
+        return data;
+    }
+
+    public void setData(IpData data) {
+        this.data = data;
+    }
+}
